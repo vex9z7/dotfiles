4026687ffe05612a630022ef19dd119256af2172
ThisisRito <face.tubas0p@icloud.com>
2023-11-24 15:22:27 +0900

    i3, xrandr: support screen layouts for laptop and 2 vertical

--- .config/xrandr/2vertical.sh
+++ .config/xrandr/2vertical.sh
@@ -1,4 +1,4 @@
 xrandr --output DP-3 --off
 
-xrandr --output DP-1 --scale 1x1 --pos 0x0 --rotate right --primary
-xrandr --output DP-4 --scale 1x1 --rotate right --right-of DP-1
+xrandr --output DP-1 --auto --scale 1x1 --pos 0x0 --rotate right --primary
+xrandr --output DP-4 --auto --scale 1x1 --rotate right --right-of DP-1
---
+++ .config/xrandr/laptop.sh
@@ -0,0 +1,4 @@
+xrandr --output DP-1 --off
+xrandr --output DP-4 --off
+
+xrandr --output DP-3 --auto  --scale 1x1  --pos 0x0 --primary
