4560258aac3c05b038da789c007addd5bd7fe8c8
ThisisRito <face.tubas0p@icloud.com>
2023-11-30 14:51:29 +0900

    update monitor layouts

--- .config/xrandr/2vertical.sh
+++ .config/xrandr/2vertical.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 xrandr --output DP-3 --off
 
-xrandr --output DP-1 --auto --scale 1x1 --pos 0x0 --rotate right --primary
-xrandr --output DP-4 --auto --scale 1x1 --rotate right --right-of DP-1
-
+xrandr --output DP-1 --auto --pos 0x0 --rotate right --primary --scale 1x1
+xrandr --output DP-4 --auto --rotate right --right-of DP-1 --scale 1x1
 
 notify-send "The 2-vertical layout is set."
--- .config/xrandr/laptop.sh
+++ .config/xrandr/laptop.sh
@@ -2,7 +2,7 @@
 xrandr --output DP-1 --off
 xrandr --output DP-4 --off
 
-xrandr --output DP-3 --auto  --scale 1x1  --pos 0x0 --primary
+xrandr --output DP-3 --auto  --scale 2x2  --pos 0x0 --primary
 
 
 notify-send "The laptop layout is set."
