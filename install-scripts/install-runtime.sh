ce9759128073fbd63025458c842e9775ac757936
vex9z7 <face.tubas0p@icloud.com>
2025-09-08 17:48:01 +0900

    dump

---
+++ install-scripts/install-runtime.sh
@@ -0,0 +1,21 @@
+sudo apt install dunst
+sudo apt install suckless-tools
+sudo apt install xss-lock
+sudo apt install i3lock
+
+sudo apt install blueman
+sudo apt install i3status
+sudo apt install fzf
+sudo apt install playerctl
+sudo apt install pulseaudio
+
+sudo apt install autorandr
+sudo apt install picom
+
+sudo apt install peek
+
+# copy q
+flatpak install --user flathub com.github.hluk.copyq
+
+# flameshot
+flatpak install --user flathub org.flameshot.Flameshot
