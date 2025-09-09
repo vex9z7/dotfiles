313e8a18477c4a91d943f4da86ec035e0cd9cb40
vex9z7 <face.tubas0p@icloud.com>
2025-09-09 11:52:15 +0900

    remove unused configs

--- .config/xrandr/2vertical.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-xrandr --output DP-3 --off
-
-xrandr --output DP-1 --auto --pos 0x0 --rotate right --primary --scale 1x1
-xrandr --output DP-4 --auto --rotate right --right-of DP-1 --scale 1x1
-
-notify-send "The 2-vertical layout is set."
--- .config/xrandr/laptop.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-xrandr --output DP-1 --off
-xrandr --output DP-4 --off
-
-xrandr --output DP-3 --auto  --scale 2x2  --pos 0x0 --primary
-
-
-notify-send "The laptop layout is set."
